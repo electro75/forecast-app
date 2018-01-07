eb875d0672bf0cd3a19f847fb15878fa54acdbab
Sahil Naikdhure <sanaikdhure@gmail.com>
2018-01-07 22:23:13 +0530

    adds the search bar containre

---
+++ src/containers/search-bar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+
+    constructor(props){
+        super(props)
+        
+        this.state = {
+            term: ''
+        }
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+        this.setState({ term: event.target.value })
+    }
+
+    render () {
+        return (
+            <div className= "container">
+                <div className = "row">
+                    <div className = "col-sm-4">
+                        <form className="input-group">
+                            <input className="form-control" placeholder="Enter a City" 
+                            value = {this.state.term} onChange = {this.onInputChange}/>
+                            <span className="input-group-btn">
+                                <button type="submit" className="btn btn-secondary">Submit</button>
+                            </span>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
