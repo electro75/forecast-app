66d37cec06ef7285060ca7e706edc50864d66f6b
Sahil Naikdhure <sanaikdhure@gmail.com>
2018-01-18 21:10:43 +0530

    adds connect method to the container

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
     render () {
         return (
             <table className="table table-hover">
                 <thead>
                     <tr>
                         <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
                     </tr>
                 </thead>
                 <tbody>
-
+                <tr></tr>
                 </tbody>
             </table>
         )
     }
  }
+
+ function mapStateToProps({ weather }) {
+     return { weather };    //es6 syntax
+ }
+
+ export default connect (mapStateToProps)(WeatherList);
