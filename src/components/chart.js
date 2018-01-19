e2349ede6183b85c75c2e122eaf7c77fe9995bf7
Sahil Naikdhure <sanaikdhure@gmail.com>
2018-01-19 23:51:27 +0530

    adds units

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,13 +1,19 @@
+import _ from 'lodash';
 import  React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 export default (props)=> {
 
+    function average (data) {
+        return _.round(_.sum(data)/data.length)
+    }
     return (
         <div>
             <Sparklines data= {props.data} svgHeight={80} svgWidth={180}  >
                 <SparklinesLine color= {props.color} />
+                <SparklinesReferenceLine type="avg"/>
             </Sparklines>
+            <div>{ average(props.data) } { props.units } </div>
         </div>    
     )
 
